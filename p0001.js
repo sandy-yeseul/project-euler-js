d254ecb8596c8eeeb1a700cb5173927f850caa5c
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-05 23:23:39 -0400

    add initial of p 0001

---
+++ p0001.js
@@ -0,0 +1,15 @@
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+ * The sum of these multiples is 23.
+ * 
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+/**
+ * 1. try to solve it
+ * 2. testing sheet?
+ */
+
+export default function add(){
+    
+}
