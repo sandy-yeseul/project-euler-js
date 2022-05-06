711c0e4dbdbfecba7a4a71d7e9724c2cc4893902
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-05 23:44:28 -0400

    fixing not to reach max number

--- p0001.js
+++ p0001.js
@@ -14,18 +14,20 @@ export default function findSum(max){
     // find sum of multiple 3
     let i = 0;
     let sumOf3 = 0;
-    while(i <= max){
+    while(i < max){
         if(i%3 ===0) sumOf3 = +i;
         i++;
     }
+    console.log(`sum of 3: ${sumOf3}`)
 
     // find sum of multiple 5
     i = 0;
     let sumOf5 = 0;
-    while(i <= max){
+    while(i < max){
         if(i % 5 === 0) sumOf5 = +i;
         i++;
     }
+    console.log(`sum of 5: ${sumOf5}`)
 
     return sumOf3 + sumOf5
 }
