a042e5029ec08d8a3661150881bc2d53b9d106b3
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-06 15:06:33 -0400

    adding test case for p 0002. testing even value sum only upto 20

---
+++ p0002.spec.js
@@ -0,0 +1,9 @@
+import { expect } from "chai"
+import sumEvenValues from "./p0002.js"
+
+describe("p 0002 testing", ()=>{
+    it("should sum up to 20, even-valued terms", ()=>{
+        const sum = sumEvenValues(20)
+        expect(23)
+    })
+})
