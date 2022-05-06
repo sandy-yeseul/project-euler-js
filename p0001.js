6eba73dad85658cc64c063e008d7d6b4b9b93a6d
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-05 23:40:18 -0400

    fix loop syntax

--- p0001.js
+++ p0001.js
@@ -14,7 +14,7 @@ export default function findSum(max){
     // find sum of multiple 3
     let i = 0;
     let sumOf3 = 0;
-    while(i===max){
+    while(i <= max){
         if(i%3 ===0) sumOf3++;
         i++;
     }
@@ -22,7 +22,7 @@ export default function findSum(max){
     // find sum of multiple 5
     i = 0;
     let sumOf5 = 0;
-    while(i===max){
+    while(i <= max){
         if(i % 5 === 0) sumOf5++;
         i++;
     }
