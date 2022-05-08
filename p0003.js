6e71942789fa0cf003703151682071a6f9efd14b
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-07 23:37:54 -0400

    initial for p 3 problem

---
+++ p0003.js
@@ -0,0 +1,17 @@
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ * 
+ * prime factors: 
+ * Prime factor is the factor of the given number which is a prime number. 
+ * Factors are the numbers you multiply together to get another number. 
+ * In simple words, 
+ * prime factor is finding which prime numbers multiply together to make the original number.
+ */
+
+function findPrimeFactors(num){
+    
+}
+function getLargestPF(num){
+
+}
