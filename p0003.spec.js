93f2938c861fcf1c387a870441075618e203c802
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-07 23:39:58 -0400

    add test case for p 3

---
+++ p0003.spec.js
@@ -0,0 +1,9 @@
+import { expect } from "chai"
+import getLargestPF from "./p0003"
+
+describe("testing for p 3", ()=>{
+    it("should return 29 - the largest prime factor for 13195", ()=>{
+        const res = getLargestPF(13195)
+        expect(res).to.be.eq(29)
+    })
+})
