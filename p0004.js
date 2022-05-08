e490e47040fee0c2b9d6239a0cdc6b0302ea4f5a
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-08 19:04:58 -0400

    trying to make biggest number for digit

--- p0004.js
+++ p0004.js
@@ -1,11 +1,15 @@
 /**
  * A palindromic number reads the same both ways. 
  * The largest palindrome made from the product of two 2-digit numbers is 
+ * 
  * 9009 = 91 × 99.
  * 
  * Find the largest palindrome made from the product of two 3-digit numbers.
  */
 
-export default function findPalindrome(){
-    
+export default function findPalindrome(digit){
+    const palindromeDigit = digit * 2;
+
+    const max = parseInt(Array(digit).fill(9).toLocaleString())
+    console.log(Array(digit).fill(9).toLocaleString())
 }
