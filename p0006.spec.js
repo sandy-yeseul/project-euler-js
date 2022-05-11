4f3c11c750c3869f518dd7d2baebfc240567667d
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-10 23:01:22 -0400

    intial commit for p6

---
+++ p0006.spec.js
@@ -0,0 +1,9 @@
+import { expect } from "chai"
+import { findDifferenceSumSquare } from "./p0006.js"
+
+describe("testing p 6", ()=>{
+    it("should return 3025 - 385", ()=>{
+        const res = findDifferenceSumSquare(10)
+        expect(res).to.be.eq(3025 - 385)
+    })
+})
