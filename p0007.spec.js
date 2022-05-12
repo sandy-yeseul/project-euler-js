e1c050cee6108560782fbf64f48037699867d0f8
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-11 23:22:52 -0400

    got correct answer

--- p0007.spec.js
+++ p0007.spec.js
@@ -5,4 +5,8 @@ describe("testing p 7", ()=>{
     it("should return 13 for 6th prime number", ()=>{
         expect(findNthPrimeNumber(6)).to.be.eq(13)
     })
+
+    it("should return answer", ()=>{
+        console.log(findNthPrimeNumber(10001))
+    })
 })
