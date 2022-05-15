cefb95675c7e2e472a9123b81d0118310d8647a4
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-15 15:14:12 -0400

    validating pythagoren for a, b, c - testing working

--- p0009.js
+++ p0009.js
@@ -9,6 +9,21 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
  */
 
-export function findPythagoreanTriplet(num){
-    
+export function findPythagoreanTriplet(sum){
+    /**
+     * 12
+     * a < b < c
+     * 
+     * 1 2 9
+     * 1 3 8
+     * 1 4 7
+     * 1 5 6
+     * 2 3 7
+     * 2 4 6
+     * 3 4 5
+     */
+}
+
+export function validatePythagoren(a, b, c){
+    return Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)
 }
--- p0009.spec.js
+++ p0009.spec.js
@@ -1,8 +1,32 @@
 import { expect } from "chai";
-import { findPythagoreanTriplet } from "./p0009";
+import { findPythagoreanTriplet, validatePythagoren } from "./p0009.js";
 
 describe('testing p 9', () => {
     it("should return 60", ()=>{
         expect(findPythagoreanTriplet(12)).to.be.eq(60)
+    })
+
+    it("should return 480 for 6, 8, 10", ()=>{
+        expect(findPythagoreanTriplet(24)).to.be.eq(480)
+    })
+
+    it('should return 4200 for 7, 24, 25', ()=>{
+        expect(findPythagoreanTriplet(56)).to.be.eq(4200)
+    })
+
+    it("should return true for 3, 4, 5", ()=>{
+        expect(validatePythagoren(3, 4, 5)).to.be.true;
+    })
+
+    it('should return ture for 6, 8, 10', ()=>{
+        expect(validatePythagoren(6, 8, 10)).to.be.true;
+    })
+
+    it('should return true for 7, 24, 25', ()=>{
+        expect(validatePythagoren(7, 24, 25)).to.be.true;
+    })
+    
+    it("should return false for 7, 8, 9", ()=>{
+        expect(validatePythagoren(7, 8, 9)).to.be.false;
     })
 });
