bbca942b3e3355ef9b1a00f91528d7f8eec9ce16
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-15 15:27:39 -0400

    testing for 3, 4, 5 and 6, 8, 10 and 7, 24, 25 is working but... not sure it's right

--- p0009.js
+++ p0009.js
@@ -10,6 +10,26 @@ Find the product abc.
  */
 
 export function findPythagoreanTriplet(sum){
+    let stop = false;
+    let product = 1;
+    for(let a=1; a< sum; a++){
+
+        for(let b=a+1; b< sum; b++){
+            let c = sum - (a+b);
+            if(c <= b) break;
+
+            if(validatePythagoren(a, b, c)){
+                stop = true;
+                console.log(a, b, c)
+                product = a * b * c;
+                break;
+            }
+        }
+
+        if(stop === true) break;
+    }
+
+    return product;
     /**
      * 12
      * a < b < c
@@ -18,14 +38,15 @@ export function findPythagoreanTriplet(sum){
      * 1 3 8
      * 1 4 7
      * 1 5 6
+     * 
      * 2 3 7
      * 2 4 6
+     * 
      * 3 4 5
      */
 }
 
 export function validatePythagoren(a, b, c){
-    if(a >= c || b >= c) return false;
-    if(a >= b) return false;
+    if(a >= c || b >= c || a >= b) return false;
     return Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2) && a < b < c
 }
