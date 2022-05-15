8a8055e518a701a461fc3504f8f0da0135e35a79
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-14 20:18:21 -0400

    got wrong answer

--- p0008.spec.js
+++ p0008.spec.js
@@ -5,4 +5,8 @@ describe('testing p 8', () => {
     it("should retrun 5832", ()=>{
         expect(findLargestProductInSeries(4)).to.be.eq(5832)
     })
+    
+    it('should return answer', ()=>{
+        console.log(findLargestProductInSeries(13))
+    })
 });
