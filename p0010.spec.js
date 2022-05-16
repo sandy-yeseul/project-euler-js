f5fd0c15a67780c866b358dac959770a3e7b9185
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-16 17:11:38 -0400

    initial commit for p 10

---
+++ p0010.spec.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { findPrimeSum } from "./p0010";
+
+describe('testing p 10', () => {
+    it("should return 17 for below 10", ()=>{
+        expect(findPrimeSum(10)).to.be.eq(17)
+    })
+});
