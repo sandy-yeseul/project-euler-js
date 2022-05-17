44630e09846a4f427031fb84db5b28b645cd4ad3
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-17 15:34:58 -0400

    got the answer, which is correct, but still causing timeout... need to reduce time

--- p0010.spec.js
+++ p0010.spec.js
@@ -5,7 +5,7 @@ describe('testing p 10', () => {
     it("should return 17 for below 10", ()=>{
         expect(findPrimeSum(10)).to.be.eq(17)
     })
-    it("should give me answer", ()=>{
-        console.log(findPrimeSum(2* 1000 *1000))
+    it("should give me answer, 142913828922", ()=>{
+        expect(findPrimeSum(2* 1000 *1000)).to.be.eq(142913828922)
     })
 });
