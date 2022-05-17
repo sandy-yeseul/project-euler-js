2b5c790ca3bd824f7c843aa4e7234d97b3df0ef2
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-16 22:47:33 -0400

    trying to loop once - get prime numbers and add together but still time exceeded

--- p0010.js
+++ p0010.js
@@ -6,13 +6,28 @@
  * Find the sum of all the primes below two million.
  */
 
-import { getPrimeNumbers } from "./p0005.js";
-
 export function findPrimeSum(max){
     console.time(`p 10: ${max}`)
 
-    const primeNumbers = getPrimeNumbers(max);
-    const sum = primeNumbers.reduce((prev, cur)=> prev + cur, 0)
+    const primeNumbers = [2, 3]
+    let sum = primeNumbers.reduce((prev, cur) => prev + cur, 0);
+    let num = 6
+    let pn1 = num -1, pn2 = num +1
+
+    while(pn1 <= max){
+        if(primeNumbers.find(n => pn1 % n === 0) === undefined) {
+            primeNumbers.push(pn1);
+            sum += pn1;
+        }
+        if(pn2 > max) break;
+        if(primeNumbers.find(n => pn2 % n === 0) === undefined){
+            primeNumbers.push(pn2);
+            sum += pn2;
+        }
+        
+        
+        pn1 +=6, pn2 +=6;
+    }
 
     console.timeEnd(`p 10: ${max}`)
     return sum;
