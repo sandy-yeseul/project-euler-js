93d0dd5966527d7cbebae8d727c3ca10658eb535
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-16 21:58:39 -0400

    two million stopped the code... need to fix it

--- p0010.spec.js
+++ p0010.spec.js
@@ -5,4 +5,7 @@ describe('testing p 10', () => {
     it("should return 17 for below 10", ()=>{
         expect(findPrimeSum(10)).to.be.eq(17)
     })
+    it("should give me answer", ()=>{
+        console.log(findPrimeSum(2* 1000 *1000))
+    })
 });
