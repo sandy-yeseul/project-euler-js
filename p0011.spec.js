4e16775ef1a56249e3e8f4c6036263055cec3f8e
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-18 22:26:57 -0400

    initial commit for p11

---
+++ p0011.spec.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { findLargestProductInGrid } from "./p0011.js";
+
+describe('testing p 11', () => {
+    it("should return answer", ()=>{
+        console.log(findLargestProductInGrid())
+    })
+});
