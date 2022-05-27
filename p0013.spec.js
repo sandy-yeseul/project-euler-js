179c53f6f177aee73e8c994ab894ed5ad78ea73d
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-27 15:17:05 -0400

    initial commit for p 13

---
+++ p0013.spec.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { sumLarge } from "./p0013.js";
+
+describe('testing p 13', () => {
+    it("should return answer", ()=>{
+        console.log(sumLarge())
+    })
+});
